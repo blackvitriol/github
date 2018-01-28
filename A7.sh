c79e314d0d3ed04b7bf5df40eaf2c1a7f05446ba
Ahmad M <ahmedmansoor60@gmail.com>
2018-01-28 15:40:25 +0500

    fixed

--- A7.sh
+++ A7.sh
@@ -22,7 +22,7 @@ echo "██║  ██║   ██║  ██║ ╚═╝ ██║███�
 sleep 0.5s
 echo "╚═╝  ╚═╝   ╚═╝  ╚═╝     ╚═╝╚═════╝  ╚═════╝   ╚═══╝  "
                     
-echo " 			A 7 M D 0 V   S C R I P T S  2 0 1 8 (c) "
+echo " 	A 7 M D 0 V   S C R I P T S  2 0 1 8 (c) "
 sleep 2s
 
 echo "Welcome to A7's remote login script ! Please authenticate below:"
@@ -33,29 +33,29 @@ read -sp 'Password: ' passvar
 
 echo Thank you $uservar we now have your login details...attempting to sign you in...
 
-  if [ $uservar == a7md0v ] && [ $passvar == loleasy ]; then
+if [ $uservar == a7md0v ] && [ $passvar == loleasy ]; then
     echo "$(tput setaf 2)Welcome Ahmad ! $(tput setab 7) or whoever thought it was smart to find the password $(tput sgr 0)"
-  else
-    echo "You are not authorized to sign in, sorry."
-	exit 1
 
-title="Please choose a number"
-prompt="Pick an option:"
-options=("A" "B" "C")
+	title="Please choose a number"
+	prompt="Pick an option:"
+	options=("A" "B" "C")
 
-echo "$title"
-PS3="$prompt "
-select opt in "${options[@]}" "Quit"; do 
+	echo "$title"
+	PS3="$prompt "
+	select opt in "${options[@]}" "Quit"; do 
 
-    case "$REPLY" in
+		case "$REPLY" in
 
-    1 ) echo "You picked $opt which is option $REPLY";;
-    2 ) echo "You picked $opt which is option $REPLY";;
-    3 ) echo "You picked $opt which is option $REPLY";;
+		1 ) echo "You picked $opt which is option $REPLY";;
+		2 ) echo "You picked $opt which is option $REPLY";;
+		3 ) echo "You picked $opt which is option $REPLY";;
 
-    $(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
-    *) echo "Invalid option. Try another one.";continue;;
+		$(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
+		*) echo "Invalid option. Try another one.";continue;;
 
-    esac
+		esac
+else
+echo "You are not authorized to sign in, sorry."
+exit 1
 
 done
