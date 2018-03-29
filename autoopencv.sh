d9e6054ce2519ccc1de1199ca77324debc6aca3d
Ahmad M <ahmed_mansoor_60@hotmail.com>
2018-03-29 08:56:12 +0500

    Add files via upload

---
+++ autoopencv.sh
@@ -0,0 +1,14 @@
+echo -e "\n**********************\nA7's Automated OpenCV Installer\n**********************\n"
+
+ARG CAFFE_VERSION=master
+
+# Install OpenCV
+RUN git clone --depth 1 https://github.com/opencv/opencv.git /root/opencv && \
+	cd /root/opencv && \
+	mkdir build && \
+	cd build && \
+	cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON .. && \
+	make -j"$(nproc)"  && \
+	make install && \
+	ldconfig && \
+	echo 'ln /dev/null /dev/raw1394' >> ~/.bashrc
